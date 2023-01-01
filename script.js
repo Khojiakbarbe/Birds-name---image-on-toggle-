ba0d0db6e13db272e44250b58d7bc18ab5864fa0
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-01-01 18:39:38 +0500

    .

---
+++ script.js
@@ -0,0 +1,96 @@
+
+
+document.querySelectorAll(".pic")[0].addEventListener("click" , function(){
+
+    let element = document.querySelectorAll(".pic")[0];
+    element.classList.toggle("bee");
+
+})
+document.querySelectorAll(".pic")[1].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[1];
+    element.classList.toggle("butterfly");
+})
+document.querySelectorAll(".pic")[2].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[2];
+    element.classList.toggle("cockato");
+})
+
+document.querySelectorAll(".pic")[3].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[3];
+    element.classList.toggle("eagle");
+})
+
+document.querySelectorAll(".pic")[4].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[4];
+    element.classList.toggle("parrot");
+})
+document.querySelectorAll(".pic")[5].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[5];
+    element.classList.toggle("falcon");
+})
+
+document.querySelectorAll(".pic")[6].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[6];
+    element.classList.toggle("goldfinch");
+})
+
+document.querySelectorAll(".pic")[7].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[7];
+    element.classList.toggle("goose");
+})
+
+document.querySelectorAll(".pic")[8].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[8];
+    element.classList.toggle("ostrich");
+})
+
+document.querySelectorAll(".pic")[9].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[9];
+    element.classList.toggle("pigeon");
+})
+
+document.querySelectorAll(".pic")[10].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[10];
+    element.classList.toggle("smallow");
+})
+
+document.querySelectorAll(".pic")[11].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[11];
+    element.classList.toggle("sparrow");
+})
+
+document.querySelectorAll(".pic")[12].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[12];
+    element.classList.toggle("elephant");
+})
+
+document.querySelectorAll(".pic")[13].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[13];
+    element.classList.toggle("lion");
+})
+document.querySelectorAll(".pic")[14].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[14];
+    element.classList.toggle("tiger");
+})
+
+document.querySelectorAll(".pic")[15].addEventListener("click" , function(){
+   
+    let element = document.querySelectorAll(".pic")[15];
+    element.classList.toggle("wolf");
+})
+
+
